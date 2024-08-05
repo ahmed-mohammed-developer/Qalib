a34cbcb9003cc8144c546c38d03f06da5491f1c0
ahmed <ahmed.zaakan@gmail.com>
2024-08-05 18:15:47 +0300

    Add Mobile Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import NavbarMobail from './Components/Navbar/NavbarMobail'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import LandingPage from './Components/LandingPage/LandingPage'
 import EShop from './Components/EShop/EShop'
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <NavbarMobail />
       <BrowserRouter>
       <Routes>
         <Route path="/Qalib" element={<h1>جميع القوالب</h1>} />
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import imgLogo from '../../assets/images/5.png'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
+  const handleNavbarOpen = () => {
+      setOpen(!open)
+  }
   return (
     <>
     
     <nav>
       <div className='container navbar'>
-            <img src={imgLogo} alt="logo Image" />
+        <div>
+        <i className="fa-solid fa-bars" onClick={handleNavbarOpen}></i>
+      {open? (
+         <div className="mobil-nav">
+         <ul>
+            <li>جميع القوالب</li>
+            <li>صفحات الهبوط</li>
+            <li> موقع للشركات</li>
+            <li>موقع شخصي</li>
+            <li>أخرى</li>
+          </ul>
+         </div>
+     ):null}
+        </div>
+              <div>
+              <img src={imgLogo} alt="logo Image" />
+              </div>
+              <div>
+              <i class="fa-solid fa-magnifying-glass"></i>
+              </div>
         <div className="nav-top">
           <form>
           <i class='bx bx-search'></i>
