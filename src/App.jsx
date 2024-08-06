9f40915626f74a6610617fd426558821cd98b67d
ahmed <ahmed.zaakan@gmail.com>
2024-08-06 11:34:41 +0300

    Add Baner Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Home from './Components/Home/Home'
 import LandingPage from './Components/LandingPage/LandingPage'
 import EShop from './Components/EShop/EShop'
 
@@ -10,7 +11,7 @@ const App = () => {
       <Navbar />
       <BrowserRouter>
       <Routes>
-        <Route path="/Qalib" element={<h1>جميع القوالب</h1>} />
+        <Route path="/Qalib" element={<Home />} />
         <Route path="/Qalib/LandingPage" element={<LandingPage />} />
         <Route path="/Qalib/EShop" element={<EShop />} />
         <Route path="*" element={<Navigate to="/Qalib/" />} />
---
+++ src/Components/Home/Baner/Baner.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import './Baner.css'
+import BanerJson from '../../Json/BanerJson.json'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+const Baner = () => {
+    const data = BanerJson.posts;
+    var settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        initialSlide: 0,
+        rows:1,
+        slidesPerRow:1,
+        autoplay: true, // تفعيل التمرير التلقائي
+        autoplaySpeed: 8000, // المدة الزمنية بين كل انتقال (3000 مللي ثانية = 3 ثواني
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                autoplay: true,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      };
+  return (
+    <>
+     <div className="baner">
+        <div className="container banerContainer">
+        <Slider {...settings}>
+        {data.map((item, index)=> (
+             <div className="row"  key={index}>
+                 <div className="col-xl-12 .col-lg-12 col-md-12 col-sm-12">
+                 <div className="imgBaner">
+                     <img src={item.img} alt={item.title} />
+                 </div>
+                 <div className="description-baner">
+                     <h1>{item.title}</h1>
+                     <p>{item.content}</p>
+                 </div>
+             </div>
+         </div>
+        ))}
+         </Slider>
+        </div>
+     </div>
+     </>
+  )
+}
+
+export default Baner
