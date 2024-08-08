5431dc7b0f4cb75a81119e42723f18438b663c21
ahmed <ahmed.zaakan@gmail.com>
2024-08-08 15:36:07 +0300

    Add Save Local

--- src/Components/Home/Main/Main.jsx
+++ src/Components/Home/Main/Main.jsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import './Main.css'
 import AllJson from '../../../../public/AllJson.json'
@@ -14,24 +15,99 @@ const Main = () => {
     {data.map((item, index) => (
     <div className='main' key={index}>
       <div className="container">
-      <Link to={`/Qalib/Details/${item.id}`}>
         <div className="row">
             <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12">
               <div className="card">
+              <Link to={`/Qalib/Details/${item.id}`}>
                 <img src={`${item.img}?${index}`} className="card-img-top" alt="" />
+                </Link>
+                <Link to={`/Qalib/Details/${item.id}`}>
                 <div className="card-body">
                   <h5 className="card-title">{item.title}</h5>
                   <p className="card-text">{item.content}</p>
                 </div>
+                </Link>
+                <i class="fa-regular fa-eye"></i>
+                <i class="fa-solid fa-file-arrow-down"></i>
+                <i class="fa-regular fa-bookmark"></i>
               </div>
             </div>
         </div>
-        </Link>
       </div>
     </div>
         ))};
         </>
   )
 }
+
+export default Main;
+*/
+import React, { useState, useEffect } from 'react';
+import './Main.css';
+import AllJson from '../../../../public/AllJson.json';
+import { Link } from 'react-router-dom';
+
+const Main = () => {
+  const data = AllJson.posts;
+  const [savedItems, setSavedItems] = useState([]);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('savedItems')) || [];
+    setSavedItems(saved);
+  }, []);
+
+  const saveToLocalStorage = (item) => {
+    let updatedSavedItems = [...savedItems];
+    if (savedItems.some(savedItem => savedItem.id === item.id)) {
+      updatedSavedItems = updatedSavedItems.filter(savedItem => savedItem.id !== item.id);
+      setMessage('تم إزالة العنصر من المفضلة');
+    } else {
+      updatedSavedItems.push(item);
+      setMessage('تم حفظ العنصر في المفضلة');
+    }
+    setSavedItems(updatedSavedItems);
+    localStorage.setItem('savedItems', JSON.stringify(updatedSavedItems));
+    setTimeout(() => setMessage(''), 2000); // إخفاء الرسالة بعد 2 ثانية
+  };
+
+  const isItemSaved = (item) => {
+    return savedItems.some(savedItem => savedItem.id === item.id);
+  };
+
+  return (
+    <>
+      {data.map((item, index) => (
+        <div className='main' key={index}>
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+                <div className="card" style={{ position: 'relative' }}>
+                  <Link to={`/Qalib/Details/${item.id}`}>
+                    <img src={`${item.img}?${index}`} className="card-img-top" alt="" />
+                  </Link>
+                  <Link to={`/Qalib/Details/${item.id}`}>
+                    <div className="card-body">
+                      <h5 className="card-title">{item.title}</h5>
+                      <p className="card-text">{item.content}</p>
+                    </div>
+                  </Link>
+                  <i className="fa-regular fa-eye"></i>
+                  <i className="fa-solid fa-file-arrow-down"></i>
+                  <i 
+                    className={`fa-regular fa-bookmark ${isItemSaved(item) ? 'saved' : ''}`} 
+                    onClick={() => saveToLocalStorage(item)}
+                    style={{ color: isItemSaved(item) ? 'orange' : 'inherit' }}
+                  ></i>
+                  {message && <div className="message">{message}</div>}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Main;
