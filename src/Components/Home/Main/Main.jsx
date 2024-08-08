8d06f1910aa18dc5a23179ede24af5818a15e2ac
ahmed <ahmed.zaakan@gmail.com>
2024-08-08 14:47:09 +0300

    Add url

--- src/Components/Home/Main/Main.jsx
+++ src/Components/Home/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Main.css'
-import AllJson from '../../Json/AllJson.json'
+import AllJson from '../../Details/AllJson.json'
 import { Link } from 'react-router-dom';
 
 
